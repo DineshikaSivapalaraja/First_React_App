cc4bcf5bf8cd64868445a18beb1c37411bcbb3ff
Dineshika <99dinuraja@gmail.com>
2023-03-26 00:28:34 +0530

    new file:   .gitignore 	modified:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello Friends!!!</h1>
+      <h2>I just started to learning react to Rock</h2><br/><br/><br/>
+     
+      <img src={logo} className="App-logo" alt="logo" /><br/><br/>
+        
+      <br/><br/><h2>Keep Learning</h2>
+      <h4>!!! Work hard !!!</h4>
+      <h4>!! Improve skills !!</h4>
+      <h4>! Gain Knowledge !</h4> 
+     
+    </div>
+  );
+}
+
+export default App;
